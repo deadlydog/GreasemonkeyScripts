83077176cce8fc9cc0658b012528bd441f47e7b9
deadlydog <deadlydog@hotmail.com>
2012-11-19 20:58:37 -0600

    - Fixing script to work with updated GitHub website.
    - Added @grant none for compatbility reasons since we don't use the GreaseMonkey API at all.

--- MakeGitHubPullRequestAndCommitPagesFullWidth.user.js
+++ MakeGitHubPullRequestAndCommitPagesFullWidth.user.js
@@ -2,12 +2,14 @@
 // @name           Make GitHub Pull Request and Commit pages full width
 // @namespace      https://bitbucket.org/deadlydog/greasemonkeyscripts
 // @description    Makes the GitHub Pull Request page and Commit page span the full width of the browser.
-// @include        https://github.com/*/pull/*
+// @include        https://github.com/*/pull/*/files*
 // @include        https://github.com/*/commit/*
-// @version        1.3
+// @include        https://github.com/*/commits/*
+// @grant          none
+// @version        1.4
 // ==/UserScript==
 
-elements = document.getElementsByClassName('container hentry');
+elements = document.getElementsByClassName('container');
 for (index = 0; index < elements.length; index++)
 {
 	elements[index].style.width="95%";	// Only 95% to leave room for the "add comment" tooltip icon.
