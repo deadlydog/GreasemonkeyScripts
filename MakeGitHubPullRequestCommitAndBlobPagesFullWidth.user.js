b5eb4976ff8db63c3916c83d6fe5051ac9c738c7
deadlydog <deadlydog@hotmail.com>
2013-07-11 17:34:34 -0600

    - Removed Blob script and incorporated it into the Pull Request and Commit script since they share the same template now.
    - Renamed Pull Request and Commit script file name to include Blob as well.

--- MakeGitHubPullRequestCommitAndBlobPagesFullWidth.user.js
+++ MakeGitHubPullRequestCommitAndBlobPagesFullWidth.user.js
@@ -1,16 +1,23 @@
 // ==UserScript==
-// @name           Make GitHub Pull Request and Commit pages full width
+// @name           Make GitHub Pull Request, Commit, and Blob pages full width
 // @namespace      https://bitbucket.org/deadlydog/greasemonkeyscripts
-// @description    Makes the GitHub Pull Request page and Commit page span the full width of the browser.
+// @description    Makes the GitHub Pull Request, Commit, and Blob pages span the full width of the browser, rather than maxing out at the default ~900 pixels.
 // @include        https://github.com/*/pull/*/files*
 // @include        https://github.com/*/commit/*
 // @include        https://github.com/*/commits/*
+// @include        https://github.com/*/blob/*
 // @grant          none
-// @version        1.4
+// @version        1.5
 // ==/UserScript==
 
 elements = document.getElementsByClassName('container');
 for (index = 0; index < elements.length; index++)
 {
 	elements[index].style.width="95%";	// Only 95% to leave room for the "add comment" tooltip icon.
+}
+
+elements = document.getElementsByClassName('repository-content context-loader-container');
+for (index = 0; index < elements.length; index++)
+{
+	elements[index].style.width="96%";
 }
